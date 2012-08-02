7b36d9b959f6a54bc4688ec6409d5cd10e99eff0
Vincent Garrigues <vincent.garrigues@gmail.com>
2012-08-02 23:23:25 +0200

    Initial Commit

---
+++ EVE Skills/VGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VGAppDelegate.h
+//  EVE Skills
+//
+//  Created by Vincent Garrigues on 02/08/12.
+//  Copyright (c) 2012 Vincent Garrigues. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ EVE Skills/VGAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  VGAppDelegate.m
+//  EVE Skills
+//
+//  Created by Vincent Garrigues on 02/08/12.
+//  Copyright (c) 2012 Vincent Garrigues. All rights reserved.
+//
+
+#import "VGAppDelegate.h"
+
+@implementation VGAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
